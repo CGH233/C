a0aab258859bf26cc1725490abe37405a577c624
CGH233 <1113713599@qq.com>
2017-11-20 09:45:05 +0800

    第四章

---
+++ chap04/35.c
@@ -0,0 +1,18 @@
+/* 4-5 */
+
+#include <stdio.h>
+
+int main ()
+{
+    int i, no;
+    
+    printf("请输入一个正整数:");
+    scanf("%d", &no);
+    i = 1;
+    if (no >=0){
+        while (i <= no) {
+            printf("%d", i++);
+    }   printf("\n"); }
+    else
+        return 0;
+}
