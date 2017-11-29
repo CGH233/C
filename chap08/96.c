3c473d63e893dfe24d75b479b77d084b5ce73a8b
CGH233 <1113713599@qq.com>
2017-11-29 18:57:53 +0800

    第八章

---
+++ chap08/96.c
@@ -0,0 +1,20 @@
+/* 8-10 */
+#include <stdio.h>
+int main ()
+{
+    int i,j,ch;
+    int cnt[10] = {0};
+    while ((ch = getchar())  != EOF) {
+        if (ch >= '0' && ch <= '9')
+            cnt[ch - '0']++;
+    }
+    printf("数字字符出现的次数");
+    for (i = 0; i < 10; i++){
+        printf("%d:",i);
+        for (j = 0; j < cnt[i]; j++){
+            putchar('*');    
+        }
+        puts("");
+    }
+    return 0;
+}
