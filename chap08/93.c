5e3532279f9deb98fd0f19746ca1f8dff9fef8f1
CGH233 <1113713599@qq.com>
2017-11-29 17:18:18 +0800

    第八章

---
+++ chap08/93.c
@@ -0,0 +1,23 @@
+/* 8-6 */
+#include <stdio.h>
+int factorial(int n)
+{   
+    int b = 1;
+    if (n > 0) {
+        while (n > 0) {
+        b *= n;
+        n -= 1;
+    }
+    return b;
+    }
+    else
+        return 0;
+}
+int main()
+{
+    int num;
+    printf("请输入一个整数：");
+    scanf("%d", &num);
+    printf("该整数的阶乘为%d。\n",factorial(num));
+    return 0;
+}
