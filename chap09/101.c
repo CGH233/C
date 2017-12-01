8324817ce7364e37c402f95c6e9c3e3c889294e8
CGH233 <1113713599@qq.com>
2017-12-01 15:35:41 +0800

    第九章

--- chap09/101.c
+++ chap09/101.c
@@ -5,7 +5,7 @@
 int str_char(const char s[], int c)
 {
 	int i;	
-	for (i = 0;i < n && s[i] != c; i++);
+	for (i = 0;i < n && c != s[i]; i++);
 	if (i == n)
 		i = -1;
 	return i;	
@@ -13,11 +13,12 @@ int str_char(const char s[], int c)
 int main()
 {
 	char s[n];
-	int c;
+	char c;
 	printf("请输入一个字符串");
 	scanf("%s", s);
+	getchar();
 	printf("请输入要查找的字符");
-	scanf("%d", &c);
-	printf("字符%c在下标值%d处。\n",c, str_char(s,c));
+	scanf("%c", &c);
+	printf("字符%c在下标值%d处。\n",c, str_char(s,c) + 1);
 	return 0;
 }
