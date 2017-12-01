f73e44398e9dcb0f2a2a2c2f9ebba447a3f0bf49
CGH233 <1113713599@qq.com>
2017-12-01 16:30:04 +0800

    第九章

--- chap09/101.c
+++ chap09/101.c
@@ -1,22 +1,23 @@
 /* 9-5 */
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h> 
 #define n 100
-int str_char(const char s[], int c)
+int str_char(const char s[], char c)
 {
 	int i;	
 	for (i = 0;i < n && c != s[i]; i++);
 	if (i == n)
-		i = -1;
+		i = -2;
 	return i;	
 }
 int main()
 {
-	char s[n];
+	char s[n] = {0};
 	char c;
 	printf("请输入一个字符串");
 	scanf("%s", s);
 	getchar();
+//	fflush(stdin) ; 
 	printf("请输入要查找的字符");
 	scanf("%c", &c);
 	printf("字符%c在下标值%d处。\n",c, str_char(s,c) + 1);
