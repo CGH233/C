5bb4380f3eff79d27a921a4f424fa3b52727b328
CGH233 <1113713599@qq.com>
2017-12-05 19:02:16 +0800

    第十一章

--- chap11/114.c
+++ chap11/114.c
@@ -1,17 +1,20 @@
 /* 11-2 */
-//这题没想出来。。。
 #include <stdio.h>
 int main ()
 {
-	int i,j,k;
+	int i=0,j=0,k=0;
 	char a[][5] = {"LISP", "C", "Ada"};
 	char *p[] = {"PAUL", "X", "MAX"};
-	j = sizeof(a);
-	k = sizeof(p);
-	printf("%d\n",k);
-	for (i = 0;i < j / 5; i++)
+	while (a[i][0] != 'A'){
+		j++;
+		i++;
+	}
+	i=0;
+	while (*p[i++] != 'M')
+		k++;
+	for (i = 0;i <= j; i++)
 		printf("a[%d] = \"%s\"\n", i, a[i]);
-	for (i = 0;i < k / 8 ; i++)
+	for (i = 0;i <= k; i++)
 		printf("p[%d] = \"%s\"\n", i, p[i]);
 	return 0;
 }
