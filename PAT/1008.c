0d02a6123bb3e12282f8ecbf1a43e224539b5dd5
CGH233 <1113713599@qq.com>
2018-02-17 13:05:06 +0800

    PAY

---
+++ PAT/1008.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+	int N;
+	scanf("%d",&N);
+	int a[N];
+	int M;
+	scanf("%d",&M);
+	if (M > N)
+		M = M % N;
+	int i;
+	for (i = 0;i < N;i++){
+		if ((M + i) < N)
+			scanf("%d",&a[i + M]);
+		else
+			scanf("%d",&a[i + M - N]);
+	}
+	for (i = 0;i < (N - 1);i++){
+		printf("%d ",a[i]);
+	}
+	printf("%d",a[N - 1]);
+	return 0;
+}
